0a3da9f491f2c3cf2edc1caab5fe0d6cc416b85b
musacatak <ahmetmusa.catak@gmail.com>
2022-07-09 23:56:24 +0300

    add some features

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ def addNewStudent(studentList):
     points = int(input("Point: "))
     studentList.append(tuple((name, surname, studentId, points)))
 
+def removeStudent(studentList,id):
+    for student in studentList:
+        if(id == student[2]):
+            studentList.remove(student)
+
 
 def finalTask(studentList):
     list = []
@@ -40,17 +45,42 @@ def finalTask(studentList):
     df = pd.DataFrame(list, index=indx, columns=['Student ID', 'Grade', 'Succes'])
 
     df.to_excel('studentGrades.xlsx', sheet_name=lesson + ' Grades')
+    return list
 
-
+def printStudentList(studentList):
+    indx = []
+    for i in range(len(studentList)):
+        indx.append(i + 1)
+    df = pd.DataFrame(studentList, index=indx, columns=['Name', 'Surname', 'Student ID', 'Points'])
+    print(df)
+    inp = int(input('1. continue\n'
+                '2. exit\n'))
+    if inp == 2:
+        exit()
+def printStudentGrades(studentGrade):
+    indx = []
+    for i in range(len(studentGrade)):
+        indx.append(i + 1)
+    df = pd.DataFrame(studentGrade, index=indx, columns=['Student ID', 'Grade', 'Succes'])
+    print(df)
+    inp = int(input('1. continue\n'
+                    '2. exit\n'))
+    if inp == 2:
+        exit()
+    return 'a'
 def main():
     studentList = []
+    studentGrades = []
     exit = True
     while exit:
         print(
             "1. add a new student to list:\n"
             "2. remove a student from list\n"
             "3. edit a student from a list\n"
-            "4. Create the excel file\n")
+            "4. Create the excel file\n"
+            "5. Print current student List\n"
+            "6. Print current Student Grades\n"
+            "7. Exit\n")
         operand = int(input("Please choose an operand: "))
 
         if operand == 1:
@@ -63,15 +93,29 @@ def main():
                     addNewStudent(studentList)
             elif type == 2:
                 df1 = pd.read_excel('studentsList.xlsx', sheet_name='Sayfa1')
-                print(df1)
                 studentList = df1[['Name', 'Surname', 'ID', 'Points']].values.tolist()
         elif operand == 2:
-            print('Removing will be added in a week')
+            if len(studentList) == 0:
+                print('Please fill the studentList first. There are no any student to remove')
+            else:
+                rmStId = int(input('Enter student ID to remove: '))
+                removeStudent(studentList, rmStId)
         elif operand == 3:
             print('editing will be add in a week')
         elif operand == 4:
-            finalTask(studentList)
-            exit = False
+            studentGrades = finalTask(studentList)
+        elif operand == 5:
+            if len(studentList) == 0:
+                print('Please fill the studentList first. There are no any students to print')
+            else:
+                printStudentList(studentList)
+        elif operand == 6:
+            if len(studentGrades) == 0:
+                print('Please fill the studentGrades first. There are no any data to print')
+            else:
+                printStudentGrades(studentGrades)
+        elif operand == 7:
+            break
         else:
             print('Invalid value , please enter a valid value !')
 
